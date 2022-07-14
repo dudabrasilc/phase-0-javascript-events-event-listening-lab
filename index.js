591d4b46ff8b3468ddbcbc436eea2462b2c8c6af
Maria Coitinho <brasildu@gmail.com>
2022-07-14 14:34:11 -0500

    commit tree 80411defec26bd5ab4dee1fb1b2fe8062135f483

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 function addingEventListener() {
+    const input = document.getElementById('button')
+    function clickAlert() {
+        alert('I was clicked');
+    }
+    input.addEventListener('click', clickAlert)
 }
+
+addingEventListener()
+// const input = document.getElementById('button')
+// input.addEventListener('click', (e) => alert('I was clicked'))
+
+// function clickAlert() {
+//     alert('I was clicked');
+// }
+
+// input.addEventListener('click', clickAlert)
